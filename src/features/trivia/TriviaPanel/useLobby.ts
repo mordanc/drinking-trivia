542eeaf59edeb0bc408187f3f5a7bbeaa087a93f
mordanc <iusethisacc4games@gmail.com>
2021-01-19 21:29:09 -0500

    removed some commented code

--- src/features/trivia/TriviaPanel/useLobby.ts
+++ src/features/trivia/TriviaPanel/useLobby.ts
@@ -1,4 +1,6 @@
 import { useToast } from "@chakra-ui/react";
+import userEvent from "@testing-library/user-event";
+import { connected } from "process";
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { io } from "socket.io-client";
@@ -68,7 +70,6 @@ const useLobby = () => {
           status: "error",
           description: `${name} has left the room`,
         });
-        // setToggle(!toggle);
         dispatch(removeUserFromList({ id, name }));
       }
     });
@@ -103,6 +104,10 @@ const useLobby = () => {
     socketRef.current.emit("updateSelectedCategories", selectedCategories);
   };
 
+  const selectNewHotseat = (socketId) => {
+    socketRef.current.emit("selectNewHotseat", socketId);
+  };
+
   return {
     connect,
     switchRoom,
@@ -113,105 +118,5 @@ const useLobby = () => {
     toggle,
   };
 };
-
-// const useLobby = (
-//   questionObject = {},
-//   userName = `mordan${Math.floor(Math.random() * 100)}`
-// ) => {
-//   //   const socket = io(SOCKET_SERVER_URL);
-//   const [connectedUsers, setConnectedUsers] = useState<any[]>([]);
-//   const [hosting, setHosting] = useState(false);
-//   const socketRef = useRef<any>();
-
-//   const currentQuestion = useSelector(selectQuestion);
-
-//   const toast = useToast();
-
-//   const dispatch = useDispatch();
-
-//   //   useEffect(() => {
-//   //     socketRef.current = io(SOCKET_SERVER_URL);
-//   //   });
-
-//   const connect = () => {
-//     socketRef.current = io(SOCKET_SERVER_URL);
-//     socketRef.current.emit("addUser", userName);
-
-//     socketRef.current.on("updateChat", (user, msg) => {
-//       console.log(msg);
-//       toast({
-//         title: "Success",
-//         description: msg,
-//         status: "success",
-//         duration: 3000,
-//         isClosable: true,
-//       });
-
-//       if (msg.includes("joined")) {
-//         setConnectedUsers([...connectedUsers, user]);
-//         console.log(connectedUsers);
-//       }
-//     });
-
-//     socketRef.current.on("hostMessage", (isHosting) => {
-//       setHosting(isHosting);
-//     });
-
-//     socketRef.current.on("selectChoice", (answer) => {
-//       if (!hosting) {
-//         dispatch(setSelectedAnswer(answer));
-//       }
-//     });
-
-//     socketRef.current.on("sendNewQuestion", () => {
-//       if (hosting) {
-//         socketRef.current.emit("updateCurrentQuestion", currentQuestion);
-//         console.log("sending question,", currentQuestion);
-//       }
-//     });
-
-//     socketRef.current.on("receiveNewQuestion", (question) => {
-//       if (!hosting) {
-//         dispatch(updateQuestion(question));
-//       }
-//     });
-//   };
-
-//   const addUser = () => {
-//     socketRef.current.emit("addUser", userName);
-//   };
-
-//   const switchRoom = (roomName) => {
-//     setConnectedUsers([]);
-//     socketRef.current.emit("switchRoom", roomName);
-
-//     if (!hosting) {
-//       socketRef.current.emit("requestCurrentQuestion");
-//     }
-//   };
-
-//   const emitSelectAnswer = (answer) => {
-//     socketRef.current.emit("selectChoice", answer);
-//   };
-
-//   const emitNewQuestion = (question) => {
-//     socketRef.current.emit("sendNewQuestion", question);
-//   };
-
-//   const requestNewQuestion = () => {
-//     socketRef.current.emit("requestCurrentQuestion");
-//   };
-
-//   return {
-//     addUser,
-//     switchRoom,
-//     connect,
-//     connectedUsers,
-//     hosting,
-//     emitSelectAnswer,
-//     emitNewQuestion,
-//     requestNewQuestion,
-//   };
-// };
 
 export default useLobby;
