e1a6eff8971a920ccbaa1ee1cb566e53f7a2e08d
mordanc <iusethisacc4games@gmail.com>
2021-01-20 12:40:46 -0500

    moved navbar to its own folder

---
+++ src/features/trivia/Navbar/Navbar.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  Input,
+  useDisclosure,
+  Text,
+  Flex,
+  Container,
+  IconButton,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+import hamburger from "../../app/images/menu-white-48dp.svg";
+
+import SettingsDrawer from "../Settings/SettingsDrawer";
+import { useSelector } from "react-redux";
+import { selectIsHost, selectRoomName } from "../triviaSlice";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const isHost = useSelector(selectIsHost);
+  const roomName = useSelector(selectRoomName);
+
+  const btnRef = React.useRef();
+
+  const size = useBreakpointValue({
+    sm: "sm",
+    md: "md",
+    lg: "lg",
+    xl: "xl",
+  });
+
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      className="navbar"
+      p="1rem"
+    >
+      <SettingsDrawer isOpen={isOpen} onClose={onClose} btnRef={btnRef} />
+      <IconButton
+        onClick={onOpen}
+        aria-label="Search database"
+        icon={<HamburgerIcon />}
+      />
+    </Flex>
+  );
+}
